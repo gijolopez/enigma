d50c487c9e29acd250487eea15b91598d6f8b13b
gijolopez <glopez22@me.com>
2018-02-05 17:42:06 -0700

    added groups_of_four method and index_position along with alphabet key.

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -2,11 +2,26 @@ class Encryptor
   attr_reader :message
   def initialize(message)
     @message = message
+    @alphabet =["a", "b", "c", "d", "e", "f", "g",
+                "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z", "0", "1",
+                "2", "3", "4", "5", "6", "7", "8",
+                "9", " ", ".", ","]
   end
 
   def split_chars
     @message.downcase.chars
   end
 
+  def groups_of_four
+    sub_strings = []
+    split_chars.each_slice(4){|letter| sub_strings << letter}
+    sub_strings
+  end
 
+  def index_position
+    @alphabet.index(groups_of_four[0][0])
+
+  end
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -23,4 +23,27 @@ class EncryptorTest < Minitest::Test
 
     assert_equal ['h', 'e', 'l', 'l', 'o'], encryptor.split_chars
   end
+
+  def test_group_chars_in_groups_of_four
+    encryptor = Encryptor.new('The boy is crossing the street')
+    encryptor.split_chars
+
+    expected = [["t", "h", "e", " "],
+                 ["b", "o", "y", " "],
+                 ["i", "s", " ", "c"],
+                 ["r", "o", "s", "s"],
+                 ["i", "n", "g", " "],
+                 ["t", "h", "e", " "],
+                 ["s", "t", "r", "e"],
+                 ["e", "t"]]
+
+    assert_equal expected, encryptor.groups_of_four
+  end
+
+  def test_if_letter_can_be_found_in_alphabet
+    encryptor = Encryptor.new("t")
+    encryptor.split_chars
+
+    assert_equal 19, encryptor.index_position
+  end
 end
