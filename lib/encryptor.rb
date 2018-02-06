edc22f574d1bfa9b5750d730ddb64e6e0c13ce6c
hmesander <haleymesander@gmail.com>
2018-02-06 14:34:38 -0700

    Refactors encryptor tests and code to correct linter warnings.

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,16 +1,18 @@
 require_relative 'offset'
 
+# Encryptor class implementation code.
 class Encryptor
   attr_reader :message
   def initialize(message, key)
     @message = message
     @offset = Offset.new(key)
-    @alphabet =["a", "b", "c", "d", "e", "f", "g",
-                "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u",
-                "v", "w", "x", "y", "z", "0", "1",
-                "2", "3", "4", "5", "6", "7", "8",
-                "9", " ", ".", ","]
+    @alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
+                 'g', 'h', 'i', 'j', 'k', 'l',
+                 'm', 'n', 'o', 'p', 'q', 'r',
+                 's', 't', 'u', 'v', 'w', 'x',
+                 'y', 'z', '0', '1', '2', '3',
+                 '4', '5', '6', '7', '8', '9',
+                 ' ', '.', ',']
   end
 
   def split_chars
@@ -19,7 +21,7 @@ class Encryptor
 
   def groups_of_four
     sub_strings = []
-    split_chars.each_slice(4){|letter| sub_strings << letter}
+    split_chars.each_slice(4) { |letter| sub_strings << letter }
     sub_strings
   end
 
@@ -28,28 +30,30 @@ class Encryptor
   end
 
   def rotate_alphabet_by_a
-      alphabet_a = ["a", "b", "c", "d", "e", "f", "g",
-                  "h", "i", "j", "k", "l", "m", "n",
-                  "o", "p", "q", "r", "s", "t", "u",
-                  "v", "w", "x", "y", "z", "0", "1",
-                  "2", "3", "4", "5", "6", "7", "8",
-                  "9", " ", ".", ","]
-      @offset.a_rotation.times do
-        alphabet_a.push(alphabet_a.shift)
-      end
-      alphabet_a
+    alphabet_a = ['a', 'b', 'c', 'd', 'e', 'f',
+                  'g', 'h', 'i', 'j', 'k', 'l',
+                  'm', 'n', 'o', 'p', 'q', 'r',
+                  's', 't', 'u', 'v', 'w', 'x',
+                  'y', 'z', '0', '1', '2', '3',
+                  '4', '5', '6', '7', '8', '9',
+                  ' ', '.', ',']
+    @offset.a_rotation.times do
+      alphabet_a.push(alphabet_a.shift)
+    end
+    alphabet_a
   end
 
   def rotate_alphabet_by_b
-      alphabet_b = ["a", "b", "c", "d", "e", "f", "g",
-                  "h", "i", "j", "k", "l", "m", "n",
-                  "o", "p", "q", "r", "s", "t", "u",
-                  "v", "w", "x", "y", "z", "0", "1",
-                  "2", "3", "4", "5", "6", "7", "8",
-                  "9", " ", ".", ","]
-      @offset.a_rotation.times do
-        alphabet_b.push(alphabet_b.shift)
-      end
-      alphabet_b
+    alphabet_b = ['a', 'b', 'c', 'd', 'e', 'f',
+                  'g', 'h', 'i', 'j', 'k', 'l',
+                  'm', 'n', 'o', 'p', 'q', 'r',
+                  's', 't', 'u', 'v', 'w', 'x',
+                  'y', 'z', '0', '1', '2', '3',
+                  '4', '5', '6', '7', '8', '9',
+                  ' ', '.', ',']
+    @offset.a_rotation.times do
+      alphabet_b.push(alphabet_b.shift)
+    end
+    alphabet_b
   end
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -4,8 +4,8 @@ require './lib/encryptor'
 require 'minitest/autorun'
 require 'minitest/pride'
 
+# Tests Encryptor class.
 class EncryptorTest < Minitest::Test
-
   def test_it_exist
     encryptor = Encryptor.new('Hello', '11111')
 
@@ -21,45 +21,39 @@ class EncryptorTest < Minitest::Test
   def test_split_message_into_character_array
     encryptor = Encryptor.new('Hello', '11111')
 
-    assert_equal ['h', 'e', 'l', 'l', 'o'], encryptor.split_chars
+    assert_equal %w[h e l l o], encryptor.split_chars
   end
 
-  def test_group_chars_in_groups_of_four
-    encryptor = Encryptor.new('The boy is crossing the street', '11111')
+  def test_group_chars_in_fours
+    encryptor = Encryptor.new('The boy laughs', '11111')
     encryptor.split_chars
 
-    expected = [["t", "h", "e", " "],
-                 ["b", "o", "y", " "],
-                 ["i", "s", " ", "c"],
-                 ["r", "o", "s", "s"],
-                 ["i", "n", "g", " "],
-                 ["t", "h", "e", " "],
-                 ["s", "t", "r", "e"],
-                 ["e", "t"]]
+    expected = [['t', 'h', 'e', ' '],
+                ['b', 'o', 'y', ' '],
+                ['l', 'a', 'u', 'g'],
+                ['h', 's']]
 
     assert_equal expected, encryptor.groups_of_four
   end
 
   def test_if_letter_can_be_found_in_alphabet
-    encryptor = Encryptor.new("t", '11111')
+    encryptor = Encryptor.new('t', '11111')
     encryptor.split_chars
 
     assert_equal 19, encryptor.index_position
   end
 
   def test_that_alphabet_can_be_rotated_forward
-    encryptor = Encryptor.new("message", '11111')
+    encryptor = Encryptor.new('message', '11111')
 
-    expected = ["q", "r", "s", "t", "u", "v", "w", "x",
-                "y", "z", "0", "1", "2", "3", "4",
-                "5", "6", "7", "8", "9", " ", ".",
-                ",", "a", "b", "c", "d", "e", "f",
-                "g", "h", "i", "j", "k", "l", "m",
-                "n", "o", "p"]
+    expected = ['s', 't', 'u', 'v', 'w', 'x',
+                'y', 'z', '0', '1', '2', '3', '4',
+                '5', '6', '7', '8', '9', ' ', '.',
+                ',', 'a', 'b', 'c', 'd', 'e', 'f',
+                'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r']
     actual = encryptor.rotate_alphabet_by_b
 
     assert_equal expected, actual
   end
-
-
 end
