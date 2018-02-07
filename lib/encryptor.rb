40ab482c1892602f4318ee09d588abb5755b1a35
hmesander <haleymesander@gmail.com>
2018-02-06 17:16:17 -0700

    Adds hash methods.

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -3,16 +3,9 @@ require_relative 'offset'
 # Encryptor class implementation code.
 class Encryptor
   attr_reader :message
-  def initialize(message, key)
+  def initialize(message)
     @message = message
-    @offset = Offset.new(key)
-    @alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
-                 'g', 'h', 'i', 'j', 'k', 'l',
-                 'm', 'n', 'o', 'p', 'q', 'r',
-                 's', 't', 'u', 'v', 'w', 'x',
-                 'y', 'z', '0', '1', '2', '3',
-                 '4', '5', '6', '7', '8', '9',
-                 ' ', '.', ',']
+    @offset = Offset.new('00000')
   end
 
   def split_chars
@@ -25,35 +18,62 @@ class Encryptor
     sub_strings
   end
 
-  def index_position
-    @alphabet.index(groups_of_four[0][0])
+  def alphabet
+    ['a', 'b', 'c', 'd', 'e', 'f',
+     'g', 'h', 'i', 'j', 'k', 'l',
+     'm', 'n', 'o', 'p', 'q', 'r',
+     's', 't', 'u', 'v', 'w', 'x',
+     'y', 'z', '0', '1', '2', '3',
+     '4', '5', '6', '7', '8', '9',
+     ' ', '.', ',']
+  end
+
+  def rotate_alphabet_by_b
+    alphabet_b = alphabet
+    @offset.b_rotation.times do
+      alphabet_b.push(alphabet_b.shift)
+    end
+    alphabet_b
+  end
+
+  def rotate_alphabet_by_c
+    alphabet_c = alphabet
+    @offset.c_rotation.times do
+      alphabet_c.push(alphabet_c.shift)
+    end
+    alphabet_c
+  end
+
+  def rotate_alphabet_by_d
+    alphabet_d = alphabet
+    @offset.d_rotation.times do
+      alphabet_d.push(alphabet_d.shift)
+    end
+    alphabet_d
   end
 
   def rotate_alphabet_by_a
-    alphabet_a = ['a', 'b', 'c', 'd', 'e', 'f',
-                  'g', 'h', 'i', 'j', 'k', 'l',
-                  'm', 'n', 'o', 'p', 'q', 'r',
-                  's', 't', 'u', 'v', 'w', 'x',
-                  'y', 'z', '0', '1', '2', '3',
-                  '4', '5', '6', '7', '8', '9',
-                  ' ', '.', ',']
+    alphabet_a = alphabet
     @offset.a_rotation.times do
       alphabet_a.push(alphabet_a.shift)
     end
     alphabet_a
   end
 
-  def rotate_alphabet_by_b
-    alphabet_b = ['a', 'b', 'c', 'd', 'e', 'f',
-                  'g', 'h', 'i', 'j', 'k', 'l',
-                  'm', 'n', 'o', 'p', 'q', 'r',
-                  's', 't', 'u', 'v', 'w', 'x',
-                  'y', 'z', '0', '1', '2', '3',
-                  '4', '5', '6', '7', '8', '9',
-                  ' ', '.', ',']
-    @offset.a_rotation.times do
-      alphabet_b.push(alphabet_b.shift)
-    end
-    alphabet_b
+  def hash_a
+    Hash[alphabet.zip(rotate_alphabet_by_a)]
   end
+
+  def hash_b
+    Hash[alphabet.zip(rotate_alphabet_by_b)]
+  end
+
+  def hash_c
+    Hash[alphabet.zip(rotate_alphabet_by_c)]
+  end
+
+  def hash_d
+    Hash[alphabet.zip(rotate_alphabet_by_d)]
+  end
+
 end
--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -7,25 +7,25 @@ require 'minitest/pride'
 # Tests Encryptor class.
 class EncryptorTest < Minitest::Test
   def test_it_exist
-    encryptor = Encryptor.new('Hello', '11111')
+    encryptor = Encryptor.new('Hello')
 
     assert_instance_of Encryptor, encryptor
   end
 
   def test_message_argument
-    encryptor = Encryptor.new('Hello', '11111')
+    encryptor = Encryptor.new('Hello')
 
     assert_equal 'Hello', encryptor.message
   end
 
   def test_split_message_into_character_array
-    encryptor = Encryptor.new('Hello', '11111')
+    encryptor = Encryptor.new('Hello')
 
     assert_equal %w[h e l l o], encryptor.split_chars
   end
 
   def test_group_chars_in_fours
-    encryptor = Encryptor.new('The boy laughs', '11111')
+    encryptor = Encryptor.new('The boy laughs')
     encryptor.split_chars
 
     expected = [['t', 'h', 'e', ' '],
@@ -36,24 +36,36 @@ class EncryptorTest < Minitest::Test
     assert_equal expected, encryptor.groups_of_four
   end
 
-  def test_if_letter_can_be_found_in_alphabet
-    encryptor = Encryptor.new('t', '11111')
-    encryptor.split_chars
-
-    assert_equal 19, encryptor.index_position
-  end
-
   def test_that_alphabet_can_be_rotated_forward
-    encryptor = Encryptor.new('message', '11111')
+    encryptor = Encryptor.new('message')
 
-    expected = ['s', 't', 'u', 'v', 'w', 'x',
-                'y', 'z', '0', '1', '2', '3', '4',
-                '5', '6', '7', '8', '9', ' ', '.',
-                ',', 'a', 'b', 'c', 'd', 'e', 'f',
-                'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r']
+    expected = ['f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's',
+                't', 'u', 'v', 'w',
+                'x', 'y', 'z', '0', '1', '2', '3',
+                '4', '5', '6', '7', '8', '9', ' ',
+                '.', ',', 'a', 'b', 'c', 'd', 'e']
     actual = encryptor.rotate_alphabet_by_b
 
     assert_equal expected, actual
   end
+
+  def test_that_hash_can_be_created
+    skip
+    encryptor = Encryptor.new('message')
+
+    expected =
+    actual = encryptor.hash_a
+
+    assert_equal expected, actual
+  end
+
+  # def test_for_encrypt_method
+  #   encryptor = Encryptor.new('messagemessage')
+  #
+  #   expected = 'tjuwhlgtjuwhlg'
+  #   actual = encryptor.encrypt
+  #
+  #   assert_equal expected, actual
+  # end
 end
